f2cac124b4b4fdb0f534830a115fa4792bb2b298
jmmunoza <69641274+jmmunoza@users.noreply.github.com>
2020-10-02 11:15:42 -0500

    Update Decision_Tree.py

--- Proyecto Final/Decision_Tree.py
+++ Proyecto Final/Decision_Tree.py
@@ -10,7 +10,12 @@ def create_tree_score(file_training):
     x_train, y_train = Data_Processing.data_score_processing(file_training)
 
     #       Creación del arbol de decisión.
-    decision_tree = DecisionTreeClassifier()
+    decision_tree = DecisionTreeClassifier(criterion='entropy', splitter='best',
+                                           min_samples_split=2, min_samples_leaf=0.0000001,
+                                           min_weight_fraction_leaf=0.0, max_features=3,
+                                           random_state=12, max_leaf_nodes=None,
+                                           min_impurity_decrease=0, class_weight="balanced",
+                                           presort='deprecated', ccp_alpha=0.0)
 
     #       Entrenamiento del arbol de desición con los datos de entrenamiento
     decision_tree = decision_tree.fit(x_train, y_train)
@@ -26,7 +31,12 @@ def create_tree_socioeconomic(file_training):
     x_train, y_train = Data_Processing.data_socioeconomic_processing(file_training)
 
     #       Creación del arbol de decisión.
-    decision_tree = DecisionTreeClassifier()
+    decision_tree = DecisionTreeClassifier(criterion='entropy', splitter='best',
+                                           min_samples_split=2, min_samples_leaf=0.0000001,
+                                           min_weight_fraction_leaf=0.0, max_features=3,
+                                           random_state=12, max_leaf_nodes=None,
+                                           min_impurity_decrease=0, class_weight="balanced",
+                                           presort='deprecated', ccp_alpha=0.0)
 
     #       Entrenamiento del arbol de desición con los datos de entrenamiento
     decision_tree = decision_tree.fit(x_train, y_train)
@@ -42,7 +52,12 @@ def create_tree_score_socioeconomic(file_training):
     x_train, y_train = Data_Processing.data_score_socioeconomic_processing(file_training)
 
     #       Creación del arbol de decisión.
-    decision_tree = DecisionTreeClassifier()
+    decision_tree = DecisionTreeClassifier(criterion='entropy', splitter='best',
+                                           min_samples_split=2, min_samples_leaf=0.0000001,
+                                           min_weight_fraction_leaf=0.0, max_features=3,
+                                           random_state=12, max_leaf_nodes=None,
+                                           min_impurity_decrease=0, class_weight="balanced",
+                                           presort='deprecated', ccp_alpha=0.0)
 
     #       Entrenamiento del arbol de desición con los datos de entrenamiento
     decision_tree = decision_tree.fit(x_train, y_train)
