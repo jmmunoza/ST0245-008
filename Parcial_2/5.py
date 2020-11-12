0c1156b0484c92a0f9f45a2c9c8a3912a4fa055a
jmmunoza <69641274+jmmunoza@users.noreply.github.com>
2020-11-12 11:59:30 -0500

    Add files via upload

---
+++ Parcial_2/5.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import networkx as nx
+import matplotlib.pyplot as plt
+
+A = np.matrix([[0,1,1,1,1,0],
+               [1,0,0,1,1,0],
+               [1,0,0,1,1,0],
+               [0,1,1,0,0,0],
+               [0,0,0,0,0,0],
+               [0,0,0,0,0,0]])
+
+G = nx.from_numpy_matrix(A)
+nx.draw(G)
+plt.show()
